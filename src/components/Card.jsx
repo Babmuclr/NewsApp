8ba17fca8af630c2cf87766ec2596848350b470b
Babmuclr <it.onepiece.balsreal@gmail.com>
2021-12-04 23:08:10 +0900

    heroku

---
+++ src/components/Card.jsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+import reutersURL from '../assets/reuters.png';
+import bloombergURL from '../assets/bloomberg.png';
+import cnbcURL from '../assets/cnbc.png';
+import thestreetURL from '../assets/thestreet.png';
+import foxbusinessURL from '../assets/foxibusiness.png';
+
+export default function MediaCard(props) {
+    let src;
+    switch (props.source) {
+        case "Reuters":
+            src=reutersURL;
+            break;
+        case "Bloomberg":
+            src=bloombergURL;
+            break;
+        case "CNBC":
+            src=cnbcURL;
+            break;
+        case "TheStreet":
+            src=thestreetURL;
+            break;
+        case "Fox Business":
+            src=foxbusinessURL;
+            break;
+        default:
+            src=bloombergURL;
+            break;
+        }
+    const date = new Date(props.pubDate.toDate());
+  return (
+    <Card sx={{ maxWidth: 350, maxHeight: 400 }}>
+    <CardActionArea  href={props.link}>
+        <CardMedia
+            component="img"
+            height="150"
+            src={src}
+            alt={props.id}
+        />
+        <CardContent>
+            <Typography gutterBottom variant="body1" component="div">
+                {props.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                {date.toDateString()} {props.source} {props.stock} 
+            </Typography>
+        </CardContent>
+    </CardActionArea>
+    </Card>
+  );
+}
